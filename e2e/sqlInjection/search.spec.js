802c75e010ada4d4c0150ae5a258f4928bd8f100
VolodjaPopov <volodja.popov@automaticity.rs>
2026-08-13 14:48:08 +0200

    Added SQL Injection test for Search, with list of patterns to test

---
+++ e2e/sqlInjection/search.spec.js
@@ -0,0 +1,16 @@
+import { test } from "../../modules/base";
+import pages from "../../fixtures/pages.json";
+
+let page;
+
+test.describe("SQL injection - GET Search", () => {
+  test.beforeEach(async ({ wpage, login }) => {
+    page = wpage;
+    await login.loginUser({});
+    await page.goto(`${process.env.BASE_URL}${pages.sqlInjectionGetSearch}`);
+  });
+
+  test("Seach GET", async ({ sqlInjection }) => {
+    await sqlInjection.verifySearchField();
+  });
+});
--- modules/sqlInjection.js
+++ modules/sqlInjection.js
@@ -1,4 +1,7 @@
 import { GeneralFunctions } from "./generalFunctions";
+import { log } from "../modules/base";
+import messages from "../fixtures/messages.json";
+import sqlPatterns from "../fixtures/sqlInjectionPatterns.json";
 
 let generalFunctions;
 
@@ -9,5 +12,38 @@ export class SQLInjection {
     this.movieSearchField = page.locator('[id="title"]');
     this.searchButton = page.locator('[value="search"]:has-text("Search")');
     this.movieTable = page.locator('[id="table_yellow"]');
+  }
+
+  async verifySearchField(patterns = sqlPatterns.injectionsForGetSelect) {
+    for (const pattern of patterns) {
+      await this.movieSearchField.fill(pattern);
+      await this.searchButton.click();
+      let tableRows = await this.movieTable.locator("tr").all();
+
+      // If the message for no movies available is visible then there are no SQL injections to be found
+      if (
+        await this.movieTable
+          .locator(`td:has-text('${messages.noMoviesAvailable}')`)
+          .isVisible()
+      )
+        log.plain("No SQL Injection error found for this pattern");
+      // If the table grows by some rows it means a request went through, which is worth investigating
+      else if ((await tableRows.length) > 2)
+        log.warn(
+          "Possible SQL Injection issue found for the following pattern\n" +
+            pattern +
+            "\n" +
+            "please review manually"
+        );
+      /* If the table doesn't change but no 'No movies' message is found that means some other error 
+        (possibly relating to SQL) is visible, alo worth investigating */ else {
+        log.info(
+          "Possible error relating to SQL found for the following pattern\n" +
+            pattern +
+            "\n" +
+            "please review manually"
+        );
+      }
+    }
   }
 }
