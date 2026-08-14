538e9834873bbc5a90c818d8cae0b8adb9e23707
VolodjaPopov <volodja.popov@automaticity.rs>
2026-08-14 12:34:53 +0200

    Renamed and reordered tests in spec files

--- e2e/htmlInjection/htmlLowSecurity.spec.js
+++ e2e/htmlInjection/htmlLowSecurity.spec.js
@@ -3,48 +3,37 @@ import pages from "../../fixtures/pages.json";
 
 let page;
 
-test.describe("HTML injection - Reflected (GET)", () => {
+test.describe("HTML Injection tests", () => {
   test.beforeEach(async ({ wpage, login }) => {
     page = wpage;
     await login.loginUser({});
+  });
+
+  test("Reflected GET (Field entry)", async ({ htmlInjection }) => {
     await page.goto(
       `${process.env.BASE_URL}${pages.defaultPages.htmlInjectionReflectedGet}`
     );
-  });
-
-  test("Low security, GET (Field entry)", async ({ htmlInjection }) => {
     await htmlInjection.verifyNameFields({});
   });
 
-  test("Low security, GET (URL entry)", async ({ htmlInjection }) => {
+  test("Reflected GET (URL entry)", async ({ htmlInjection }) => {
+    await page.goto(
+      `${process.env.BASE_URL}${pages.defaultPages.htmlInjectionReflectedGet}`
+    );
     await htmlInjection.verifyURLInjections({});
   });
-});
 
-test.describe("HTML injection - Reflected (POST)", () => {
-  test.beforeEach(async ({ wpage, login }) => {
-    page = wpage;
-    await login.loginUser({});
+  test("Reflected POST", async ({ htmlInjection }) => {
     await page.goto(
       `${process.env.BASE_URL}${pages.defaultPages.htmlInjectionReflectedPost}`
     );
-  });
-
-  test("Low security, POST", async ({ htmlInjection }) => {
     await htmlInjection.verifyNameFields({});
   });
-});
 
-test.describe("HTML injection - Stored", () => {
-  test.beforeEach(async ({ wpage, login }) => {
-    page = wpage;
-    await login.loginUser({});
+  test("Stored (Blog)", async ({ htmlInjection }) => {
     await page.goto(
       `${process.env.BASE_URL}${pages.defaultPages.htmlInjectionStored}`
     );
-  });
-
-  test("Low security, Stored", async ({ htmlInjection }) => {
     await htmlInjection.verifyTableEntryInjection({});
   });
 });
