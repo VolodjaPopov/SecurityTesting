4ead25586af653a90ab542fc3cc758c2d72da897
VolodjaPopov <volodja.popov@automaticity.rs>
2026-08-14 12:04:05 +0200

    SQL Injection POST test and GET url test, small improvements

--- e2e/sqlInjection/search.spec.js
+++ e2e/sqlInjection/search.spec.js
@@ -1,32 +1,44 @@
 import { test } from "../../modules/base";
+import sqlPatterns from "../../fixtures/sqlInjectionPatterns.json";
 import pages from "../../fixtures/pages.json";
 
 let page;
 
-test.describe("SQL injection - GET Search", () => {
+test.describe("SQL Injection tests", () => {
   test.beforeEach(async ({ wpage, login }) => {
     page = wpage;
     await login.loginUser({});
+  });
+
+  test("Search GET", async ({ sqlInjection }) => {
     await page.goto(
       `${process.env.BASE_URL}${pages.defaultPages.sqlInjectionGetSearch}`
     );
-  });
-
-  test("Seach GET", async ({ sqlInjection }) => {
     await sqlInjection.verifySearchField();
   });
-});
 
-test.describe("SQL injection - GET Select", () => {
-  test.beforeEach(async ({ wpage, login }) => {
-    page = wpage;
-    await login.loginUser({});
+  test("Search GET (URL Injection)", async ({ sqlInjection }) => {
     await page.goto(
-      `${process.env.BASE_URL}${pages.defaultPages.sqlInjectionGetSelect}`
+      `${process.env.BASE_URL}${pages.defaultPages.sqlInjectionGetSearch}`
     );
+    await sqlInjection.verifyURLInjections({
+      patterns: sqlPatterns.injectionsForGetString,
+      parameter: "title",
+      submitValid: pages.validGETRequestPages.getSearchValidOption,
+    });
+  });
+
+  test("Seach POST", async ({ sqlInjection }) => {
+    await page.goto(
+      `${process.env.BASE_URL}${pages.defaultPages.sqlInjectionPostSearch}`
+    );
+    await sqlInjection.verifySearchField();
   });
 
   test("Select GET", async ({ sqlInjection }) => {
+    await page.goto(
+      `${process.env.BASE_URL}${pages.defaultPages.sqlInjectionGetSelect}`
+    );
     await sqlInjection.verifyURLInjections({});
   });
 });
