6c1779370af176d3f1f3fdcb365f2b2f4f25a09b
VolodjaPopov <volodja.popov@automaticity.rs>
2026-08-19 12:20:03 +0200

    HTML Injections medium security tests

--- e2e/htmlInjection/htmlMediumSecurity.spec.js
+++ e2e/htmlInjection/htmlMediumSecurity.spec.js
@@ -26,4 +26,50 @@ test.describe("HTML Injection tests (Medium security)", () => {
       await htmlInjection.verifyNameFields({ encoded: true });
     });
   });
+
+  test("Reflected GET (URL entry)", async ({
+    generalFunctions,
+    htmlInjection,
+  }) => {
+    await test.step("Go to the 'HTML Injection Reflected (GET)' page", async () => {
+      await generalFunctions.visitPage(
+        pages.defaultPages.htmlInjectionReflectedGet
+      );
+    });
+
+    await test.step("Verify unencoded injections don't work with this security level", async () => {
+      await htmlInjection.verifyURLInjections({});
+    });
+
+    await test.step("Verify HTML Injections with encoded requests (through URL)", async () => {
+      await htmlInjection.verifyURLInjections({ encoded: true });
+    });
+  });
+
+  test("Reflected POST", async ({ generalFunctions, htmlInjection }) => {
+    await test.step("Go to the 'HTML Injection Reflected (POST)' page", async () => {
+      await generalFunctions.visitPage(
+        pages.defaultPages.htmlInjectionReflectedGet
+      );
+    });
+
+    await test.step("Verify unencoded injections don't work with this security level", async () => {
+      await htmlInjection.verifyNameFields({});
+    });
+
+    await test.step("Verify HTML Injections with encoded requests", async () => {
+      await htmlInjection.verifyNameFields({ encoded: true });
+    });
+  });
+
+  // There is an issue with bWAPP where medium security is like high for this page, terefore this test is commented out
+
+  // test("Stored (Blog)", async ({ generalFunctions, htmlInjection }) => {
+  //   await test.step("Go to the 'HTML Injection Reflected (POST)' page", async () => {
+  //     await generalFunctions.visitPage(pages.defaultPages.htmlInjectionStored);
+  //   });
+
+  //   await htmlInjection.verifyTableEntryInjection({});
+  //   await htmlInjection.verifyTableEntryInjection({ encoded: true });
+  // });
 });
