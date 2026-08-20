71ba77a3ae45492b8eeda80528a534d9ce0ecaaf
VolodjaPopov <volodja.popov@automaticity.rs>
2026-08-20 12:11:05 +0200

    SQL Injection medium security level tests

---
+++ e2e/sqlInjection/sqlMediumSecurity.spec.js
@@ -0,0 +1,90 @@
+import { test } from "../../modules/base";
+import sqlPatterns from "../../fixtures/sqlInjectionPatterns.json";
+import pages from "../../fixtures/pages.json";
+
+/* In this file some tests are vulnerable to SQL Injections for this security leve, but most
+   are not. These tests have been left to show that basic and more advanced SQL Injection patterns 
+   do not work for well secured apps. */
+
+test.describe("SQL Injection tests (Medium Security)", () => {
+  test.beforeEach(async ({ login }) => {
+    await test.step("Log in with medium security level", async () => {
+      await login.loginUser({ securityLevel: "medium" });
+    });
+  });
+
+  test("Search GET", async ({ generalFunctions, sqlInjection }) => {
+    await test.step("Go to the 'SQL Injection GET/Search' page", async () => {
+      await generalFunctions.visitPage(
+        pages.defaultPages.sqlInjectionGetSearch
+      );
+    });
+
+    await test.step("Verify and log SQL injections for the 'Search movies' field", async () => {
+      await sqlInjection.verifySearchField({});
+    });
+  });
+
+  test("Search GET (URL Injection)", async ({
+    generalFunctions,
+    sqlInjection,
+  }) => {
+    await test.step("Go to the 'SQL Injection GET/Search' page", async () => {
+      await generalFunctions.visitPage(
+        pages.defaultPages.sqlInjectionGetSearch
+      );
+    });
+
+    await test.step("Verify and log SQL injections using the URL", async () => {
+      await sqlInjection.verifyURLInjections({
+        patterns: sqlPatterns.injectionsForGetString,
+        parameter: "title",
+        submitValid: pages.validGETRequestPages.getSearchValidOption,
+      });
+    });
+  });
+
+  test("Seach POST", async ({ generalFunctions, sqlInjection }) => {
+    await test.step("Go to the 'SQL Injection POST/Search' page", async () => {
+      await generalFunctions.visitPage(
+        pages.defaultPages.sqlInjectionPostSearch
+      );
+    });
+
+    await test.step("Verify and log SQL injections for the 'Search movies' field", async () => {
+      await sqlInjection.verifySearchField({});
+    });
+  });
+
+  test("Select GET", async ({ generalFunctions, sqlInjection }) => {
+    await test.step("Go to the 'SQL Injection GET/Select' page", async () => {
+      await generalFunctions.visitPage(
+        pages.defaultPages.sqlInjectionGetSelect
+      );
+    });
+
+    await test.step("Verify and log SQL injections using the URL", async () => {
+      await sqlInjection.verifyURLInjections({});
+    });
+  });
+
+  test("Blind Boolean", async ({ generalFunctions, sqlInjection }) => {
+    await test.step("Go to the 'SQL Injection POST/Search' page", async () => {
+      await generalFunctions.visitPage(pages.defaultPages.sqlInjectionBoolean);
+    });
+
+    await test.step("Verify and log SQL injections for the 'Search movies' field", async () => {
+      await sqlInjection.verifySearchField({ messagesToCheck: "boolean" });
+    });
+  });
+
+  test("Stored (Blog)", async ({ generalFunctions, htmlInjection }) => {
+    await test.step("Go to the 'SQL Injection - Stored (Blog) page", async () => {
+      await generalFunctions.visitPage(pages.defaultPages.sqlInjectionBlog);
+    });
+
+    await test.step("Verify and log HTML Injections for the table", async () => {
+      await htmlInjection.verifyInjectedWriteScriptInTable({});
+    });
+  });
+});
